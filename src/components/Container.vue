bdb8ee91e09a98b58a4098c24ea9fc059bd953f2
donghyun960215 <tlzma789@naver.com>
2023-07-03 20:54:03 +0900

    23.07.03

---
+++ src/components/Container.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Post :postdata = "postdata[i]" v-for="(post,i) in postdata" :key="post"/>
+  </div>
+
+</template>
+
+<script>
+import Post from './Post.vue'
+export default {
+  name: 'container',
+  components: {
+    Post
+  },
+  props: {
+    postdata: Array,
+  }
+}
+</script>
+
+<style>
+
+</style>
